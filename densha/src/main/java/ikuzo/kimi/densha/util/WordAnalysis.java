8210c3477e853aa62d6c4ff964c1fdb28ce098f5
kita <kita@kita-PC>
2017-04-25 08:48:29 +0900

    asd

--- densha/src/main/java/ikuzo/kimi/densha/util/WordAnalysis.java
+++ densha/src/main/java/ikuzo/kimi/densha/util/WordAnalysis.java
@@ -5,7 +5,7 @@ package ikuzo.kimi.densha.util;
 public class WordAnalysis 
 {
    public int Analysis(String text){
-	    
+	     
 	   /*    위험도의 수치를 나타낸다. 
 	    이 수치가 최근 게시물 20개에 있어서 15가 넘는다면 연착이나 사고가 있다고 가정을한다.*/
 	   int degree = 0;
